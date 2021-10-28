148755b0d3a9405a2e8b94ba4e8d6f90e1fa92e1
ayusisinha <ayusisinha26@gmailcom>
2021-10-28 22:38:06 +0530

    Initial

---
+++ Phase_1_Assignments/src/Assignments/Array2D.java
@@ -0,0 +1,16 @@
+package Assignments;
+
+public class Array2D {
+	 
+    public static void main(String[] args) 
+    {  
+        int[][] arr = { { 1, 2 }, { 3, 4 } }; 
+  
+        for (int i = 0; i < 2; i++) {                // for row
+            for (int j = 0; j < 2; j++) {            // for column
+                System.out.print(arr[i][j] + " "); 
+            } 
+            System.out.println(); 
+        } 
+    } 
+} 
